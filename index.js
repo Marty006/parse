282067a08631a6e62936fcda3754ee0397b2412b
Willian Ribeiro Angelo <agfoccus@gmail.com>
2016-06-04 15:21:49 -0300

    - ES6 Version - Install Factory - Public folder - AWS S3 - Mailgun Email

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express        = require('express');
-const ParseServer    = require('parse-server').ParseServer;
-const S3Adapter      = require('parse-server').S3Adapter;
-const expressLayouts = require('express-ejs-layouts');
+'use strict';
+const express     = require('express');
+const ParseServer = require('parse-server').ParseServer;
+const S3Adapter   = require('parse-server').S3Adapter;
 const bodyParser     = require('body-parser');
 const cookieParser   = require('cookie-parser');
 const methodOverride = require('method-override');
 const cookieSession  = require('cookie-session');
+const path           = require('path');
 
 // Parse configuration
 const databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 const serverUrl   = process.env.SERVER_URL || 'http://localhost:1337/parse';
 const appId       = process.env.APP_ID || 'myAppId';
 const masterKey   = process.env.MASTER_KEY || 'myMasterKey';
-const restApiKey  = process.env.MASTER_REST_KEY ||'myRestApiKey';
-const appName     = process.env.APP_NAME ||'nearme';
+const restApiKey  = process.env.MASTER_REST_KEY || 'myRestApiKey';
+const appName     = process.env.APP_NAME || 'nearme';
 
 // Mailgun configuration
 const apiKey      = process.env.MAILGUN_API_KEY || 'YOUR_MAILGUN_API_KEY';
@@ -57,15 +58,14 @@ const api = new ParseServer({
 
 const app = express();
 
-// app.set('view engine', 'ejs');
-// app.set('views', 'views');
+// Serve static assets from the /public folder
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
 
 // Serve the Parse API on the /parse URL prefix
 const mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
 
-app.use(express.static('public'));
-app.use(expressLayouts);
 app.use(cookieParser());
 app.use(methodOverride());
 
@@ -87,98 +87,45 @@ var isNotInstalled = function (req, res, next) {
 
     var query = new Parse.Query(Parse.Role);
     query.equalTo('name', 'Admin');
-    query.first().then(function (adminRole) {
+    query.first()
+         .then(adminRole=> {
 
-        if (!adminRole) {
-            return Parse.Promise.error({
-                message: 'Admin Role not found',
-                code: 5000
-            });
-        }
+             if (!adminRole) {
+                 return Parse.Promise.error({
+                     message: 'Admin Role not found',
+                     code: 5000
+                 });
+             }
 
-        var userRelation = adminRole.relation('users');
-        return userRelation.query().count({useMasterKey: true});
-    }).then(function (count) {
-
-        if (count === 0) {
-            next();
-        } else {
-            req.session = null;
-            res.redirect('/login');
-        }
-    }, function (error) {
-        if (error.code === 5000) {
-            next();
-        } else {
-            req.session = null;
-            res.redirect('/login');
-        }
-    })
-}
-
-var isAdmin = function (req, res, next) {
-
-    var objUser;
-
-    return Parse.Cloud.httpRequest({
-        url: serverUrl + '/users/me',
-        headers: {
-            'X-Parse-Application-Id': appId,
-            'X-Parse-REST-API-Key': restApiKey,
-            'X-Parse-Session-Token': req.session.token
-        }
-    }).then(function (userData) {
-
-        objUser = Parse.Object.fromJSON(userData.data);
-
-        var query = new Parse.Query(Parse.Role);
-        query.equalTo('name', 'Admin');
-        query.equalTo('users', objUser);
-        return query.first();
-
-    }).then(function (isAdmin) {
-
-        if (!isAdmin) {
-            return Parse.Promise.error();
-        }
-
-        req.user = objUser;
-        return next();
-
-    }).then(null, function () {
-        req.session = null;
-        res.redirect('/login');
-    });
-}
-
-var isNotAuthenticated = function (req, res, next) {
-
-    Parse.Cloud.httpRequest({
-        url: serverUrl + '/users/me',
-        headers: {
-            'X-Parse-Application-Id': appId,
-            'X-Parse-REST-API-Key': restApiKey,
-            'X-Parse-Session-Token': req.session.token
-        }
-    }).then(function (userData) {
-        res.redirect('/dashboard/places');
-    }, function (error) {
-        next();
-    });
+             let userRelation = adminRole.relation('users');
+             return userRelation.query().count({useMasterKey: true});
+         })
+         .then(count=> {
+             if (count === 0) {
+                 next();
+             } else {
+                 req.session = null;
+                 res.redirect('/login');
+             }
+         }, error=> {
+             if (error.code === 5000) {
+                 next();
+             } else {
+                 req.session = null;
+                 res.redirect('/login');
+             }
+         })
 }
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-app.get('/install', isNotInstalled, function (req, res) {
-    res.render('install');
-});
 
-app.post('/install', [urlencodedParser, isNotInstalled], function (req, res) {
+app.post('/install', [urlencodedParser, isNotInstalled], (req, res) => {
 
-    var name                 = req.body.name.trim();
-    var username             = req.body.username.toLowerCase().trim();
-    var password             = req.body.password.trim();
-    var passwordConfirmation = req.body.passwordConfirmation.trim();
+    let name                 = req.body.name.trim();
+    let username             = req.body.username.toLowerCase().trim();
+    let password             = req.body.password.trim();
+    let passwordConfirmation = req.body.passwordConfirmation.trim();
 
     if (!name) {
         return res.render('install', {
@@ -210,7 +157,7 @@ app.post('/install', [urlencodedParser, isNotInstalled], function (req, res) {
 
     var roles = [];
 
-    var roleACL = new Parse.ACL();
+    let roleACL = new Parse.ACL();
     roleACL.setPublicReadAccess(true);
 
     var role = new Parse.Role('Admin', roleACL);
@@ -218,7 +165,7 @@ app.post('/install', [urlencodedParser, isNotInstalled], function (req, res) {
     var role = new Parse.Role('User', roleACL);
     roles.push(role);
 
-    var user = new Parse.User();
+    let user = new Parse.User();
     user.set('name', name);
     user.set('username', username);
     user.set('email', username);
@@ -226,95 +173,31 @@ app.post('/install', [urlencodedParser, isNotInstalled], function (req, res) {
     user.set('roleName', 'Admin');
     user.set('photoThumb', undefined);
 
-    var query = new Parse.Query(Parse.Role);
+    let query = new Parse.Query(Parse.Role);
 
-    query.find().then(function (objRoles) {
-        return Parse.Object.destroyAll(objRoles, {useMasterKey: true});
-    }).then(function () {
-        return Parse.Object.saveAll(roles);
-    }).then(function () {
-        return user.signUp();
-    }).then(function (objUser) {
-        objUser.setACL(new Parse.ACL(objUser));
-        objUser.save(null, {useMasterKey: true});
-        req.session.user  = objUser;
-        req.session.token = objUser.getSessionToken();
-        res.redirect('/dashboard/places');
-    }, function (error) {
-        res.render('install', {
-            flash: error.message,
-            input: req.body
-        });
-    });
+    query.find()
+         .then(objRoles=>Parse.Object.destroyAll(objRoles, {useMasterKey: true}))
+         .then(()=>Parse.Object.saveAll(roles))
+         .then(()=>user.signUp())
+         .then(objUser=> {
+             objUser.setACL(new Parse.ACL(objUser));
+             objUser.save(null, {useMasterKey: true});
+             req.session.user  = objUser;
+             req.session.token = objUser.getSessionToken();
+             res.redirect('/dashboard/places');
+         }, error=> {
+             res.render('install', {
+                 flash: error.message,
+                 input: req.body
+             });
+         });
 });
 
+// Parse Server plays nicely with the rest of your web routes
 app.get('/', function (req, res) {
-    res.redirect('/login');
+    res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
-app.get('/login', isNotAuthenticated, function (req, res) {
-    res.render('login');
-});
-
-app.get('/reset-password', isNotAuthenticated, function (req, res) {
-    res.render('reset-password');
-});
-
-app.get('/dashboard/places', isAdmin, function (req, res) {
-    res.render('places');
-});
-
-app.get('/dashboard/categories', isAdmin, function (req, res) {
-    res.render('categories');
-});
-
-app.get('/dashboard/users', isAdmin, function (req, res) {
-    res.render('users');
-});
-
-app.get('/dashboard/reviews', isAdmin, function (req, res) {
-    res.render('reviews');
-});
-
-// Logs in the user
-app.post('/login', [urlencodedParser, isNotAuthenticated], function (req, res) {
-
-    var username = req.body.username;
-    var password = req.body.password;
-
-    Parse.User.logIn(username, password).then(function (user) {
-
-        var query = new Parse.Query(Parse.Role);
-        query.equalTo('name', 'Admin');
-        query.equalTo('users', user);
-        query.first().then(function (isAdmin) {
-
-            if (!isAdmin) {
-                res.render('login', {
-                    flash: 'Not Authorized'
-                });
-            } else {
-                req.session.user  = user;
-                req.session.token = user.getSessionToken();
-                res.redirect('/dashboard/places');
-            }
-
-        }, function (error) {
-            res.render('login', {
-                flash: error.message
-            });
-        });
-    }, function (error) {
-        res.render('login', {
-            flash: error.message
-        });
-    });
-});
-
-app.get('/logout', isAdmin, function (req, res) {
-    req.session = null;
-    res.redirect('/login');
-});
 
 var port = process.env.PORT || 1337;
 app.listen(port, function () {
