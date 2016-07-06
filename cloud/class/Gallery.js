04755cea8b4d993d820315d2fcb77851e1b6801b
Willian Ribeiro Angelo <agfoccus@gmail.com>
2016-07-06 16:40:41 -0300

    It is no longer necessary to create an icon to use the map

--- cloud/class/Gallery.js
+++ cloud/class/Gallery.js
@@ -92,11 +92,6 @@ function beforeSave(req, res) {
         return Image.saveImage(base64);
     }).then(savedFile => {
         gallery.set('imageThumb', savedFile);
-        return Image.resize(imageUrl, 64, 64);
-    }).then(base64=> {
-        return Image.saveImage(base64);
-    }).then(savedFile=> {
-        gallery.set('icon', savedFile);
         res.success();
     }, res.error);
 }
